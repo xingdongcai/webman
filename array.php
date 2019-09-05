04a67398d01a73a7fc7e9318abc2cd506b52f32f
xcai0009@student.monash.edu <xcai0009@student.monash.edu>
2019-09-05 14:04:41 +1000

    database correction

--- array.php
+++ array.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli-connect("130.194.7.82","s29211530","monash00","fit2103");
+    $conn = mysqli-connect("130.194.7.82","s29211530","monash00","fit2104");
     $query="SELECT * FROM category";
     $result = mysqli_query($conn,$query);
     ?>
