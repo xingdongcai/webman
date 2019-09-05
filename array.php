ba316d476f01ca85c8b3955fed0f0d3bd1d5228b
xcai0009@student.monash.edu <xcai0009@student.monash.edu>
2019-09-05 14:26:13 +1000

    show table

--- array.php
+++ array.php
@@ -1,5 +1,28 @@
 <?php
-    $conn = mysqli-connect("130.194.7.82","s29211530","monash00","fit2104");
-    $query="SELECT * FROM category";
-    $result = mysqli_query($conn,$query);
-    ?>
+$servername = "130.194.7.82";
+$username = "s29211530";
+$password = "monash00";
+$dbname = "s29211530";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, description FROM category";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>DESCRIPTION</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["description"]." </td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
