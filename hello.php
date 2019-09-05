4b5ca0147a1eed5ad39483703cdf18a4b9d06f1d
xcai0009@student.monash.edu <xcai0009@student.monash.edu>
2019-09-05 13:28:05 +1000

    change date info

--- hello.php
+++ hello.php
@@ -1,6 +1,3 @@
 <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+echo "Today is " . date("d/m/Y") . "<br>";
 ?>
