debfa259d6dcf5f33df748e1c490f6374df8c1e3
xcai0009@student.monash.edu <xcai0009@student.monash.edu>
2019-09-06 12:10:04 +1000

    add one id

--- array.php
+++ array.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 <?php
 $servername = "130.194.7.82";
 $username = "s29211530";
@@ -11,11 +13,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+
 $sql = "SELECT id, description FROM category";
 $result = $conn->query($sql);
 
+
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>DESCRIPTION</th></tr>";
+    echo "</table> <tr><th>ID</th><th>DESCRIPTION</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>".$row["id"]."</td><td>".$row["description"]." </td></tr>";
@@ -26,3 +31,5 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+<script type="text/javascript" src="RowColours.js"></script>
