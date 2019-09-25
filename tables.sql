980e9afc36655cb8888bf0702d7ae68c636d76c0
Xingdong Cai <xcai0009@student.monash.edu>
2019-09-26 01:29:26 +1000

    connection php file added

--- tables.sql
+++ tables.sql
@@ -1,26 +1,6 @@
-create table customer
-(
-    customer_id int auto_increment
-        primary key,
-    first_name varchar(20) not null,
-    last_name varchar(20) not null,
-    password int(20) null,
-    mobile_no int(20) null
-);
+INSERT INTO admin(uname, pword) VALUES ('admin',SHA2('admin',0));
 
-create table product
-(
-    product_id int(20) not null,
-    product_name varchar(25) not null,
-    product_desc varchar(500) null,
-    unit_price double(20,0) null,
-    kit_id int(20) not null,
-    constraint Product_product_id_uindex
-        unique (product_id)
-);
 
-alter table product
-    add primary key (product_id);
 
 
 
