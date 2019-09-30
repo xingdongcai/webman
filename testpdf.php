8bfcaa32a1d06fa02d95ac8b42d438f262a5d82e
Xingdong Cai <xcai0009@student.monash.edu>
2019-10-01 01:25:00 +1000

    test

--- testpdf.php
+++ testpdf.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 $pdf = new TCPDF('L', 'mm', 'A4', true);
 $pdf->setHeaderFont(array('helvetica', 'B',24));
 $pdf->setHeaderData(null,null,'My First PDF Document','');
-$pdf->SetFont(array('helvetica'),'',16);
+$pdf->SetFont(array('Arial'),'',16);
 $pdf->AddPage();
 $pdf->Cell(40,10,'First Cell - no border',0,1);
 $pdf->Cell(100,10,'Second Cell - border/centred', 1,1,'C');
