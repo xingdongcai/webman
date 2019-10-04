cf41b069dd92fe1a46ae398be380fd85e030a997
Xingdong Cai <xcai0009@student.monash.edu>
2019-10-04 16:30:25 +1000

    template client updated

--- clients/CustModify.php
+++ clients/CustModify.php
@@ -1,17 +1,14 @@
 <?php
 ob_start();
+include('../templateTop.html');
 ?>
-<html>
-<head><title></title></head>
-<link rel="stylesheet" type="text/css" href="style.css">
-<body>
+
 <script language="javascript">
     function confirm_delete()
     {
         window.location='CustModify.php?clientid=<?php echo $_GET["clientid"]; ?>&Action=ConfirmDelete';
     }
 </script>
-<center><h3>Customer Modification</h3></center>
 <?php
 include("../connection.php");
 $dsn= "mysql:host=$Host;dbname=$DB";
@@ -27,63 +24,65 @@ switch($strAction)
 {
 case "Update":
     ?>
-    <form method="post" action="CustModify.php?clientid=<?php echo $_GET["clientid"]; ?>&Action=ConfirmUpdate">
-        <center>Customer details amendment<br /></center><p />
-        <table align="center" cellpadding="3">
-            <tr />
-            <td><b>Cust. No.</b></td>
-            <td><?php echo $row->client_id; ?></td>
-            </tr>
-            <tr>
-                <td><b>Cust. Firstname</b></td>
-                <td><input type="text" name="fname" size="30" value="<?php echo $row->client_gname; ?>"></td>
-            </tr>
-            <tr>
-                <td><b>Cust. Surname</b></td>
-                <td><input type="text" name="sname" size="30" value="<?php echo $row->client_fname; ?>"></td>
-            </tr>
-            <tr>
-                <td><b>Cust. Address</b></td>
-                <td><input type="text" name="street" size="40" value="<?php echo $row->client_street; ?>"></td>
-            </tr>
-            <tr>
-                <td><b>Cust. suburb</b></td>
-                <td><input type="text" name="suburb" size="10" value="<?php echo $row->client_suburb; ?>"></td>
-            </tr>
-            <tr>
-                <td><b>State</b></td>
-                <td><input type="text" name="state" size="10" value="<?php echo $row->client_state; ?>">
-                </td>
-            </tr>
-            <tr>
-                <td><b>Postcode</b></td>
-                <td><input type="text" name="postcode" size="10" value="<?php echo $row->client_pc; ?>">
-                </td>
-            </tr>
-            <tr>
-                <td><b>Email</b></td>
-                <td><input type="text" name="email" size="30" value="<?php echo $row->client_email; ?>">
-                </td>
-            </tr>
-            <tr>
-                <td><b>Mobile</b></td>
-                <td><input type="text" name="mobile" size="15"
-                           value="<?php echo $row->client_mobile; ?>"></td>
-            </tr>
-            <tr>
-                <td><b>Mailing List</b></td>
-                <td><input type="number" name="mailinglist" size="5"
-                           value="<?php echo $row->client_mailinglist; ?>"></td>
-            </tr>
-        </table>
-        <br/>
-        <table align="center">
-            <tr>
-                <td><input type="submit" value="Update Customer"></td>
-                <td><input type="button" value="Return to List" OnClick="window.location='index.php'"></td>
-            </tr>
-        </table>
-    </form>
+    <div class="container-fluid">
+        <form method="post" action="CustModify.php?clientid=<?php echo $_GET["clientid"]; ?>&Action=ConfirmUpdate">
+            <center><h3>Customer details amendment</h3><br /></center><p />
+            <table align="center" cellpadding="3">
+                <tr />
+                <td><b>Cust. No.</b></td>
+                <td><?php echo $row->client_id; ?></td>
+                </tr>
+                <tr>
+                    <td><b>Cust. Firstname</b></td>
+                    <td><input type="text" name="fname" size="30" value="<?php echo $row->client_gname; ?>"></td>
+                </tr>
+                <tr>
+                    <td><b>Cust. Surname</b></td>
+                    <td><input type="text" name="sname" size="30" value="<?php echo $row->client_fname; ?>"></td>
+                </tr>
+                <tr>
+                    <td><b>Cust. Address</b></td>
+                    <td><input type="text" name="street" size="40" value="<?php echo $row->client_street; ?>"></td>
+                </tr>
+                <tr>
+                    <td><b>Cust. suburb</b></td>
+                    <td><input type="text" name="suburb" size="10" value="<?php echo $row->client_suburb; ?>"></td>
+                </tr>
+                <tr>
+                    <td><b>State</b></td>
+                    <td><input type="text" name="state" size="10" value="<?php echo $row->client_state; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Postcode</b></td>
+                    <td><input type="text" name="postcode" size="10" value="<?php echo $row->client_pc; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Email</b></td>
+                    <td><input type="text" name="email" size="30" value="<?php echo $row->client_email; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Mobile</b></td>
+                    <td><input type="text" name="mobile" size="15"
+                               value="<?php echo $row->client_mobile; ?>"></td>
+                </tr>
+                <tr>
+                    <td><b>Mailing List</b></td>
+                    <td><input type="number" name="mailinglist" size="5"
+                               value="<?php echo $row->client_mailinglist; ?>"></td>
+                </tr>
+            </table>
+            <br/>
+            <table align="center">
+                <tr>
+                    <td><input type="submit" value="Update Customer"></td>
+                    <td><input type="button" value="Return to List" OnClick="window.location='index.php'"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
     <?php
     break;
 
@@ -103,24 +102,28 @@ case "ConfirmUpdate":
 
 case "Delete":
     ?>
-    <center>Confirm deletion of the following customer record<br /></center><p />
-    <table align="center" cellpadding="3">
-        <tr />
-        <td><b>Cust. No.</b></td>
-        <td><?php echo $row->client_id; ?></td>
-        </tr>
-        <tr>
-            <td><b>Name</b></td>
-            <td><?php echo "$row->client_gname $row->client_fname"; ?></td>
-        </tr>
-    </table>
-    <br/>
-    <table align="center">
-        <tr>
-            <td><input type="button" value="Confirm" OnClick="confirm_delete();">
-            <td><input type="button" value="Cancel" OnClick="window.location='index.php'"></td>
-        </tr>
-    </table>
+    <div class="container-fluid">
+        <h3>Confirm deletion of the following customer record</h3>
+        <hr>
+        <table>
+            <tr>
+                <td><b>Cust. No.</b></td>
+                <td><?php echo $row->client_id; ?></td>
+            </tr>
+            <tr>
+                <td><b>Name</b></td>
+                <td><?php echo "$row->client_gname $row->client_fname"; ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><input type="button" value="Confirm" OnClick="confirm_delete();">
+                <td><input type="button" value="Cancel" OnClick="window.location='index.php'"></td>
+            </tr>
+        </table>
+    </div>
+
     <?php
     break;
 
@@ -130,20 +133,22 @@ $stmt = $dbh->prepare($query);
 if($stmt->execute())
 {
 ?>
-<center>
-    The following customer record has been successfully deleted<p />
-    <?php
-    echo "Customer No. $row->client_id $row->client_gname $row->client_fname";
-    echo "</center><p />";
-    }
-    else
-    {
-        echo "<center>Error deleting customer record<p /></center>";
-    }
-    echo "<center><input type='button' value='Return to List' OnClick='window.location=\"index.php\"'></center>";
-    break;
-    }
-    $stmt->closeCursor();
-    ?>
-</body>
-</html>
+    <div class="container-fluid">
+        The following customer record has been successfully deleted<p />
+        <?php
+        echo "Customer No. $row->client_id $row->client_gname $row->client_fname";
+        echo "</center><p />";
+        }
+        else
+        {
+            echo "<center>Error deleting customer record<p /></center>";
+        }
+        echo "<center><input type='button' value='Return to List' OnClick='window.location=\"index.php\"'></center>";
+        break;
+        }
+        $stmt->closeCursor();
+        ?>
+    </div>
+
+
+<?php include('../templateBottom.html'); ?>
--- clients/list.php
+++ clients/list.php
@@ -2,45 +2,39 @@
 require "../vendor/autoload.php";
 include("../connection.php");
 include("CreatePDF.php");
+include("../templateTop.html")
 ?>
 
-<html>
-<head>
-    <title>PHP PDF Creation</title>
-    <!-- Page level plugin CSS-->
-    <link href="../vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <link href="../css/sb-admin.css" rel="stylesheet">
-</head>
-<body>
-<h1>Create PDF</h1>
+<div class="container-fluid">
+    <h1>Create PDF</h1>
+    <?php
+    $dsn= "mysql:host=$Host;dbname=$DB";
+    $dbh = new PDO($dsn,$UName,$PWord);
+
+    $stmt = $dbh->prepare("SELECT * FROM client ORDER BY client_fname");
+    $stmt->execute();
+    $allRows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Column titles
+    $header = array('Cust. No', 'Name', 'Address', 'Contact');
+    //Column Widths
+    $headerWidth=array(100,250,300,200);
+
+    //create new instance of my CreatePDF class
+    $PDF = new CreatePDF();
+
+    //pass it headers, headerWidth and data
+    $table = $PDF->CustomerPDF($header, $headerWidth, $allRows);
+
+    echo $table;
+    ?>
+    <br/>
+    <center><button class="btn-primary btn" OnClick="window.location='PDFs/Customers.pdf'">Click here to see PDF</button></center>
+    <br/>
+</div>
+
+
 <?php
-$dsn= "mysql:host=$Host;dbname=$DB";
-$dbh = new PDO($dsn,$UName,$PWord);
-
-$stmt = $dbh->prepare("SELECT * FROM client ORDER BY client_fname");
-$stmt->execute();
-$allRows=$stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//Column titles
-$header = array('Cust. No', 'Name', 'Address', 'Contact');
-//Column Widths
-$headerWidth=array(150,250,300,200);
-
-//create new instance of my CreatePDF class
-$PDF = new CreatePDF();
-
-//pass it headers, headerWidth and data
-$table = $PDF->CustomerPDF($header, $headerWidth, $allRows);
-
-echo $table;
-?>
-<br/>
-<center><button class="btn-primary btn" OnClick="window.location='PDFs/Customers.pdf'">Click here to see PDF</button></center>
-<br/>
-<?php
-//echo dirname($_SERVER["SCRIPT_FILENAME"]);
+include("../templateBottom.html")
 ?>
 
-</body>
-</html>
