4f86b15d461df6fe4e4e2ae45cb84923fa9d3c50
Xingdong Cai <xcai0009@student.monash.edu>
2019-10-04 20:58:48 +1000

    bugs fixed

---
+++ displaySource.php
@@ -0,0 +1,22 @@
+<?php
+//$file = $_GET["filename"];
+//echo "<h1>Source Code for: ".$file."</h1>";
+////open file for reading only
+//
+////retrieve table which translates HTML tags to entities, eg convert < to &lt;
+//    $line = file_get_contents($file);
+//    $trans = get_html_translation_table(HTML_ENTITIES);
+////get one line from file $fp
+//
+////performs translation/sub string replacement based on translation table
+//    $line = strtr($line,$trans);
+////replace tab character with 4 HTML spaces to maintain some formatting
+//    $line = str_replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;",$line);
+//    $line = str_replace("\n","<br/>",$line);
+////echo line to screen, followed by break
+//    echo $line."<br />";
+//
+
+$file = $_GET["filename"];
+echo "<h1>Source Code for: " . $file . "</h1>";
+highlight_file($file);
