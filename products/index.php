427afb70e26f886586b6baedc1d8b431e5fbb736
Xingdong Cai <xcai0009@student.monash.edu>
2019-10-07 21:24:09 +1100

    search ui added

--- products/index.php
+++ products/index.php
@@ -12,24 +12,35 @@ include("../templateTop.html")
 
 
         <div id="content-wrapper">
-            <form method="post" action="index.php">
+
                 <div class="container-fluid">
 
                     <!-- Breadcrumbs-->
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a class="btn-block btn btn-primary"     href="../products/add.php">Add Product</a>
+                            <div>
+                                <a class="btn-block btn btn-primary"     href="../products/add.php">Add Product</a>
+                            </div>
                         </li>
                     </ol>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <div class="input-group md-form form-sm form-1 pl-0">
+                                <div class="input-group-prepend">
+    <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+                                                                        aria-hidden="true"></i></span>
+                                </div>
+                                <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search">
+                            </div>
 
-
-
-
+                        </li>
+                    </ol>
 
 
                     <!-- DataTables -->
 
                     <div class="card mb-3">
+                        <form method="post" action="index.php">
                         <div class="card-header">
                             <i class="fas fa-table"></i>
                             Product Table
@@ -74,28 +85,22 @@ include("../templateTop.html")
                                             <td>
                                                 <a href="../products/ProductModify.php?productId=<?php echo $row[0];?>&Action=Update">Edit</a>
                                             </td>
-
                                         </tr>
                                         <?php
                                     }
                                     //$stmt->closeCursor();
                                     ?>
-
                                     </tbody>
                                 </table>
-
-
                             </div>
                         </div>
-
                         <div class="card-footer small text-muted">Famox</div>
+                        </form>
                     </div>
-
-
                 </div>
 
 
-            </form>
+
 
             <!-- /.container-fluid -->
 
