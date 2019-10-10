7facb592d2a7d9d21d576d976ed7a996bb7eeb12
jayden <jadyen@jingyu.com>
2019-10-10 14:01:26 +1100

    datatable

--- db_tables.sql
+++ db_tables.sql
@@ -7,78 +7,86 @@ drop table PROJECT;
 drop table ADMIN;
 
 
-create table CLIENT
+create table client
 (
     client_id int auto_increment
         primary key,
     client_gname varchar(50) not null,
     client_fname varchar(50) not null,
-    client_street varchar(100),
-    client_suburb varchar(50),
-    client_state varchar(6),
-    client_pc varchar(4),
-    client_email varchar(50),
-    client_mobile varchar(12),
-    client_mailinglist varchar(1)
-
-
+    client_street char(100) null,
+    client_suburb varchar(50) null,
+    client_state varchar(20) null,
+    client_pc varchar(4) null,
+    client_email varchar(50) null,
+    client_mobile varchar(12) null,
+    client_mailinglist tinyint(1) null
 );
-create table CATEGORY
+
+
+create table category
 (
     category_id int auto_increment
         primary key,
     category_name varchar(100) not null
-
 );
-create table PRODUCT
+
+create table product
 (
     product_id int auto_increment
         primary key,
     product_name varchar(100) not null,
-    product_purchase_price int,
-    product_sale_price int,
-    product_country_of_origin varchar(40)
-
+    product_purchase_price double(11,2) null,
+    product_sale_price double(11,2) null,
+    product_country_of_origin varchar(40) null
 );
-create table PRODUCT_CATEGORY
+
+
+create table product_category
 (
-    product_id int  not null,
+    product_id int not null,
     category_id int not null,
-    constraint PRODUCT_CATEGORY_id__pk
-        primary key (product_id, category_id),
-    constraint PRODUCT_CATEGORY_id_fk
-        foreign key (product_id) references PRODUCT (product_id),
+    primary key (product_id, category_id),
     constraint CATEGORY_PRODUCT_id_fk
-        foreign key (category_id) references CATEGORY (category_id)
-
-
+        foreign key (category_id) references category (category_id),
+    constraint PRODUCT_CATEGORY_id_fk
+        foreign key (product_id) references product (product_id)
 );
-create table PROJECT
+
+
+create table project
 (
-    project_id int not null primary key,
-    project_desc varchar(100) not null ,
-    project_country varchar(50),
-    project_city varchar(50)
-
-
+    project_id int auto_increment
+        primary key,
+    project_desc varchar(100) not null,
+    project_country varchar(50) null,
+    project_city varchar(50) null
 );
-create table PRODUCT_IMAGE
-    (
-        image_id int not null primary key auto_increment,
-        product_id int not null,
-        image_name  varchar(40) not null,
-        constraint PRODUCT_IMAGE_PRODUCT_id_fk
-            foreign key (product_id) references PRODUCT (product_id)
 
-);
-create table ADMIN
+
+create table product_image
 (
-    admin_id int not null primary key,
-    uname varchar(255),
-    pword  varchar(255)
-
+    image_id int auto_increment
+        primary key,
+    product_id int not null,
+    image_name varchar(40) not null,
+    constraint PRODUCT_IMAGE_PRODUCT_id_fk
+        foreign key (product_id) references product (product_id)
 );
-SELECT * FROM category WHERE category_name LIKE '%$result%' a  inner join product_category b on a.category_id=b.category_id inner join product c on b.product_id=c.product_id
+
+
+create table admin
+(
+    admin_id int auto_increment
+        primary key,
+    uname varchar(255) null,
+    pword varchar(255) null
+);
+create table searchtable
+(
+    category_id int default 0 not null,
+    category_name varchar(100) not null
+);
+
 
 
 
